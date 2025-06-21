cf1b31d3d8618eff739d47abfaad4669882f38ff
Santhosh Appan <santzapaul1990@gmail.com>
2025-06-21 11:26:26 -0400

    Add ESLint and Prettier configuration files, update Next.js config for typed routes, and add VSCode settings for formatting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import pluginCheckFile from "eslint-plugin-check-file";
+import pluginN from "eslint-plugin-n";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +12,36 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
+  {
+    files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
+    plugins: {
+      "check-file": pluginCheckFile,
+      n: pluginN,
+    },
+    rules: {
+      "prefer-arrow-callback": ["error"],
+      "prefer-template": ["error"],
+      semi: ["error"],
+      quotes: ["error", "double"],
+      "n/no-process-env": ["error"],
+      "check-file/filename-naming-convention": [
+        "error",
+        {
+          "**/*.{ts,tsx}": "KEBAB_CASE",
+        },
+        {
+          ignoreMiddleExtensions: true,
+        },
+      ],
+      "check-file/folder-naming-convention": [
+        "error",
+        {
+          "src/**/": "KEBAB_CASE",
+        },
+      ],
+    },
+  },
 ];
 
 export default eslintConfig;
