fc1df545eab1cfe8c866a62e2c2d1f8b7fc576c8
Santhosh Appan <santzapaul1990@gmail.com>
2025-06-21 09:46:47 -0400

    Create layout and footer components. Add trademark message to footer.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+
+import { Footer } from "@/components/footer";
+
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,7 +30,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+          <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+            {children}
+          </main>
+          <Footer />
+        </div>
       </body>
     </html>
   );
---
+++ src/components/footer.tsx
@@ -0,0 +1,12 @@
+export function Footer() {
+  return (
+    <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+      <p className="text-sm">
+        “Formula 1” and “F1” are registered trademarks of Formula One Licensing
+        B.V. All references to Formula 1 in this app are used for editorial,
+        informational purposes only and are not affiliated with or endorsed by
+        Formula One Licensing B.V.
+      </p>
+    </footer>
+  );
+}
