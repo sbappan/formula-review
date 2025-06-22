3fcf4898dafd3f72e4917c665090b1428b5c52ea
Santhosh Appan <santzapaul1990@gmail.com>
2025-06-22 07:24:30 -0400

    feat: Add new light background SVG and update layout to utilize it for enhanced visual appeal. Adjust body and container styles for improved layout consistency.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,11 +24,11 @@ export default function RootLayout({
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🏎️</text></svg>"
         />
       </head>
-      <body className="h-screen w-screen">
+      <body>
         <Providers>
-          <div className="grid h-full min-h-screen w-full grid-rows-[auto_1fr_auto] before:absolute before:inset-0 before:-z-10 before:bg-[url(/light-bg.svg)] before:bg-cover before:bg-repeat before:opacity-35 sm:px-20 dark:before:bg-[url(/dark-bg.svg)] dark:before:opacity-20">
+          <div className="min-h-screen w-full before:absolute before:inset-0 before:-z-10 before:bg-[url(/light-bg-2.svg)] before:bg-cover before:bg-repeat before:opacity-35 sm:px-20 dark:before:bg-[url(/dark-bg.svg)] dark:before:opacity-20">
             <Header />
-            <main className="relative row-start-2 flex h-full w-full flex-col gap-[32px] overflow-auto px-8">
+            <main className="relative flex w-full flex-col gap-[32px] px-8 py-8">
               {children}
             </main>
             <Footer />
